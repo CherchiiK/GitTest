368f7cacb89b5d2cac90e3bdc1a61e8736acaea9
CherchiiK <igordudka2033@gmail.com>
2021-07-11 18:19:15 +0300

    added second new feature

--- src/main/features/DoSmth.java
+++ src/main/features/DoSmth.java
@@ -4,4 +4,7 @@ public class DoSmth {
     public static void newFeature(){
         System.out.println("New, cool feature!");
     }
+    public static void secondNewFeature(){
+        System.out.println("Second cool feature");
+    }
 }
