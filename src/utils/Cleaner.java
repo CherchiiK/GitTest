fb36135130c15cb50f02bf3dead98e334e5b2445
CherchiiK <igordudka2033@gmail.com>
2021-07-12 09:08:16 +0300

    added Cook feature

--- src/utils/Cleaner.java
+++ src/utils/Cleaner.java
@@ -4,4 +4,9 @@ public class Cleaner {
     public static void doCleaning(){
         System.out.println("I'm cleaning");
     }
+
+    public static void doCooking(){
+        System.out.println("I'm cooking");
+    }
+
 }
