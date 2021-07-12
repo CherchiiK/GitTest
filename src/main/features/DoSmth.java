180daeb6992a92524526875cfc8befc9f82b467d
CherchiiK <igordudka2033@gmail.com>
2021-07-12 08:26:01 +0300

    fixed error in second new feature

--- src/main/features/DoSmth.java
+++ src/main/features/DoSmth.java
@@ -4,4 +4,8 @@ public class DoSmth {
     public static void newFeature(){
         System.out.println("New, cool feature!");
     }
+    private static void secondFeature(){
+        System.out.println("Second cool feature!");
+        //Now it's fixed!
+    }
 }
